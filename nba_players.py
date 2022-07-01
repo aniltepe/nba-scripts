9e952abc0c6e46010ed044a69c6dea2a3e9da4de
aniltepe <nazimatepe@gmail.com>
2022-07-01 03:07:16 +0300

    some improvements

--- nba_players.py
+++ nba_players.py
@@ -2,11 +2,16 @@ from selenium import webdriver
 import datetime
 import os
 import sqlite3
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 
 class Player:
     def __init__(self):
         self.id = None
-        self.name = None
+        self.firstname = None
+        self.lastname = None
+        self.fullname = None
         self.team = None
         self.primarypos = None
         self.secondarypos = None
@@ -21,7 +26,7 @@ class Player:
         self.pie = None
 
 dir = os.path.dirname(__file__)
-browser = webdriver.Chrome(executable_path = dir + "/chromedriver")
+driver = webdriver.Chrome(executable_path = dir + "/chromedriver")
 
 db_connection = sqlite3.connect(dir + "/nba.sqlite")
 db_cursor = db_connection.cursor()
@@ -30,9 +35,11 @@ players = []
 
 create_command = """CREATE TABLE IF NOT EXISTS player (
     id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    team TEXT NOT NULL,
-    primarypos TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    fullname TEXT NOT NULL,
+    team TEXT,
+    primarypos TEXT,
     secondarypos TEXT,
     height INTEGER NOT NULL,
     weight INTEGER NOT NULL,
@@ -45,35 +52,54 @@ create_command = """CREATE TABLE IF NOT EXISTS player (
     pie REAL)"""
 db_cursor.execute(create_command)
 
-browser.get("https://www.nba.com/players")
-browser.implicitly_wait(3)
-page_dropdown = browser.find_elements_by_class_name("DropDown_select__5Rjt0")[-1]
+driver.get("https://www.nba.com/players")
+page_dropdown = driver.find_elements_by_class_name("DropDown_select__5Rjt0")[-1]
 page_dropdown.find_elements_by_tag_name("option")[0].click()
-player_items = browser.find_elements_by_xpath("//tbody//tr")
+player_items = driver.find_elements_by_xpath("//tbody//tr")
 player_links = []
 for player_item in player_items:
     player_links.append(player_item.find_elements_by_tag_name("td")[0].find_elements_by_tag_name("a")[0].get_attribute("href"))
 for player_link in player_links:
     player = Player()
     player.id = int(player_link.split("https://www.nba.com/player/")[1].split("/")[0])
-    browser.get(player_link)
-    browser.implicitly_wait(3)
-    name_p = browser.find_elements_by_css_selector("p.PlayerSummary_playerNameText__K7ZXO")[0]
-    team_p = name_p.parent.find_elements_by_tag_name("p")[0].text
-    player.team = team_p.split("|")[0].strip()
-    pos_p = team_p.split("|")[2].strip()
-    positions = pos_p.split("-")
-    player.primarypos = positions[0]
-    player.secondarypos  = None  
-    if len(positions) == 2:
-        player.secondarypos = positions[1]
-    first_name = browser.find_elements_by_css_selector("p.PlayerSummary_playerNameText__K7ZXO")[0].text
-    last_name = browser.find_elements_by_css_selector("p.PlayerSummary_playerNameText__K7ZXO")[1].text
-    player.name = first_name.replace("'", "") + " " + last_name.replace("'", "")
+    db_cursor.execute("SELECT * FROM player WHERE id = '" + str(player.id) + "'")
+    row = db_cursor.fetchone()
+    if row:
+        continue
+    driver.get(player_link)
+    try:
+        test = driver.find_elements_by_css_selector("p.PlayerSummary_playerNameText__K7ZXO")[0]
+    except:
+        print("problem at getting page", player_link)
+        driver.get(player_link)
+    name_p = driver.find_elements_by_css_selector("p.PlayerSummary_playerNameText__K7ZXO")[0]
+    parent_div = name_p.find_element_by_xpath("./..")
+    basic_info_ps = parent_div.find_elements_by_tag_name("p")
+    if len(basic_info_ps) == 2:
+        player.team = None
+        player.primarypos = None
+        player.secondarypos  = None
+        player.firstname = basic_info_ps[0].text
+        player.lastname = basic_info_ps[1].text
+    elif len(basic_info_ps) >= 3:
+        team_p = basic_info_ps[0].text
+        player.team = team_p.split("|")[0].strip()
+        pos_p = team_p.split("|")[2].strip() if len(team_p.split("|")) == 3 else team_p.split("|")[1].strip()
+        positions = pos_p.split("-")
+        player.primarypos = positions[0]
+        player.secondarypos  = None  
+        if len(positions) == 2:
+            player.secondarypos = positions[1]
+        player.firstname = basic_info_ps[1].text
+        player.lastname = basic_info_ps[2].text
+    player.firstname = player.firstname.replace("'", "\'")
+    player.lastname = player.lastname.replace("'", "\'")
+    player.fullname = player.firstname + " " + player.lastname
     player.pie = None
-    if len(browser.find_elements_by_css_selector("p.PlayerSummary_playerStatValue__3hvQY")) >= 4:
-        player.pie = float(browser.find_elements_by_css_selector("p.PlayerSummary_playerStatValue__3hvQY")[3].text)
-    info_labels = browser.find_elements_by_css_selector("p.PlayerSummary_playerInfoValue__mSfou")
+    stat_p = driver.find_elements_by_css_selector("p.PlayerSummary_playerStatValue__3hvQY")
+    if len(stat_p) >= 4:
+        player.pie = float(stat_p[3].text)
+    info_labels = driver.find_elements_by_css_selector("p.PlayerSummary_playerInfoValue__mSfou")
     player.height = int(info_labels[0].text.split(" ")[1].replace("(", "").replace("m)", "").replace(".", ""))
     player.weight = int(info_labels[1].text.split(" ")[1].replace("(", "").replace("kg)", ""))
     player.country = info_labels[2].text
@@ -92,9 +118,12 @@ for player_link in player_links:
         player.experience = int(info_labels[7].text.split(" ")[0])
     players.append(player)
 
-browser.quit()
+    insert_command = "INSERT INTO player VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    db_cursor.execute(insert_command, (player.id, player.firstname, player.lastname, player.fullname,
+                      player.team, player.primarypos, player.secondarypos, player.height, player.weight,
+                      player.country, player.birthdate, player.draftyear, player.draftround, player.draftpick,
+                      player.experience, player.pie))
+    db_connection.commit()
 
-insert_command = "INSERT INTO player VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-for player in players:
-    db_cursor.execute(insert_command, (player.id, player.name, player.team, player.primarypos, player.secondarypos, player.height, player.weight, player.country, player.birthdate, player.draftyear, player.draftround, player.draftpick, player.experience, player.pie))
-db_connection.commit()
+
+driver.quit()
--- nba_seasons.py
+++ nba_seasons.py
@@ -3,10 +3,12 @@ import sqlite3
 from datetime import datetime
 
 class Season:
-    def __init__(self, name, startdate_rs, enddate_rs, startdate_po, enddate_po, startdate_f, enddate_f):
+    def __init__(self, name, startdate_rs, enddate_rs, startdate_pi, enddate_pi, startdate_po, enddate_po, startdate_f, enddate_f):
         self.name = name
         self.startdate_rs = startdate_rs
         self.enddate_rs = enddate_rs
+        self.startdate_pi = startdate_pi
+        self.enddate_pi = enddate_pi
         self.startdate_po = startdate_po
         self.enddate_po = enddate_po
         self.startdate_f = startdate_f
@@ -22,6 +24,8 @@ create_command = """CREATE TABLE IF NOT EXISTS season (
     name TEXT NOT NULL PRIMARY KEY,
     startdate_rs DATE NOT NULL,
     enddate_rs DATE NOT NULL,
+    startdate_pi DATE,
+    enddate_pi DATE,
     startdate_po DATE NOT NULL,
     enddate_po DATE NOT NULL,
     startdate_f DATE NOT NULL,
@@ -30,61 +34,83 @@ db_cursor.execute(create_command)
 
 
 seasons.append(Season("2003–04", datetime(2003, 10, 28).strftime("%Y-%m-%d"), datetime(2004, 4, 14).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2004, 4, 17).strftime("%Y-%m-%d"), datetime(2004, 6, 1).strftime("%Y-%m-%d"),
     datetime(2004, 6, 6).strftime("%Y-%m-%d"), datetime(2004, 6, 15).strftime("%Y-%m-%d")))
 seasons.append(Season("2004–05", datetime(2004, 11, 2).strftime("%Y-%m-%d"), datetime(2005, 4, 20).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2005, 4, 23).strftime("%Y-%m-%d"), datetime(2005, 6, 6).strftime("%Y-%m-%d"),
     datetime(2005, 6, 9).strftime("%Y-%m-%d"), datetime(2005, 6, 23).strftime("%Y-%m-%d")))
 seasons.append(Season("2005–06", datetime(2005, 11, 1).strftime("%Y-%m-%d"), datetime(2006, 4, 19).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2006, 4, 22).strftime("%Y-%m-%d"), datetime(2006, 6, 3).strftime("%Y-%m-%d"),
     datetime(2006, 6, 8).strftime("%Y-%m-%d"), datetime(2006, 6, 20).strftime("%Y-%m-%d")))
 seasons.append(Season("2006–07", datetime(2006, 10, 31).strftime("%Y-%m-%d"), datetime(2007, 4, 18).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2007, 4, 21).strftime("%Y-%m-%d"), datetime(2007, 6, 2).strftime("%Y-%m-%d"),
     datetime(2007, 6, 7).strftime("%Y-%m-%d"), datetime(2007, 6, 14).strftime("%Y-%m-%d")))
 seasons.append(Season("2007–08", datetime(2007, 10, 30).strftime("%Y-%m-%d"), datetime(2008, 4, 16).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2008, 4, 19).strftime("%Y-%m-%d"), datetime(2008, 5, 30).strftime("%Y-%m-%d"),
     datetime(2008, 6, 5).strftime("%Y-%m-%d"), datetime(2008, 6, 17).strftime("%Y-%m-%d")))
 seasons.append(Season("2008–09", datetime(2008, 10, 28).strftime("%Y-%m-%d"), datetime(2009, 4, 16).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2009, 4, 18).strftime("%Y-%m-%d"), datetime(2009, 5, 30).strftime("%Y-%m-%d"),
     datetime(2009, 6, 4).strftime("%Y-%m-%d"), datetime(2009, 6, 14).strftime("%Y-%m-%d")))
 seasons.append(Season("2009–10", datetime(2009, 10, 27).strftime("%Y-%m-%d"), datetime(2010, 4, 14).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2010, 4, 17).strftime("%Y-%m-%d"), datetime(2010, 5, 29).strftime("%Y-%m-%d"),
     datetime(2010, 6, 3).strftime("%Y-%m-%d"), datetime(2010, 6, 17).strftime("%Y-%m-%d")))
 seasons.append(Season("2010–11", datetime(2010, 10, 26).strftime("%Y-%m-%d"), datetime(2011, 4, 13).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2011, 4, 16).strftime("%Y-%m-%d"), datetime(2011, 5, 26).strftime("%Y-%m-%d"),
     datetime(2011, 5, 31).strftime("%Y-%m-%d"), datetime(2011, 6, 12).strftime("%Y-%m-%d")))
 seasons.append(Season("2011–12", datetime(2011, 12, 25).strftime("%Y-%m-%d"), datetime(2012, 4, 26).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2012, 4, 28).strftime("%Y-%m-%d"), datetime(2012, 6, 9).strftime("%Y-%m-%d"),
     datetime(2012, 6, 12).strftime("%Y-%m-%d"), datetime(2012, 6, 21).strftime("%Y-%m-%d")))
 seasons.append(Season("2012–13", datetime(2012, 10, 30).strftime("%Y-%m-%d"), datetime(2013, 4, 17).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2013, 4, 20).strftime("%Y-%m-%d"), datetime(2013, 6, 3).strftime("%Y-%m-%d"),
     datetime(2013, 6, 6).strftime("%Y-%m-%d"), datetime(2013, 6, 20).strftime("%Y-%m-%d")))
 seasons.append(Season("2013–14", datetime(2013, 10, 29).strftime("%Y-%m-%d"), datetime(2014, 4, 16).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2014, 4, 19).strftime("%Y-%m-%d"), datetime(2014, 5, 31).strftime("%Y-%m-%d"),
     datetime(2014, 6, 5).strftime("%Y-%m-%d"), datetime(2014, 6, 15).strftime("%Y-%m-%d")))
 seasons.append(Season("2014–15", datetime(2014, 10, 28).strftime("%Y-%m-%d"), datetime(2015, 4, 15).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2015, 4, 18).strftime("%Y-%m-%d"), datetime(2015, 5, 27).strftime("%Y-%m-%d"),
     datetime(2015, 6, 4).strftime("%Y-%m-%d"), datetime(2015, 6, 16).strftime("%Y-%m-%d")))
 seasons.append(Season("2015–16", datetime(2015, 10, 27).strftime("%Y-%m-%d"), datetime(2016, 4, 13).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2016, 4, 16).strftime("%Y-%m-%d"), datetime(2016, 5, 30).strftime("%Y-%m-%d"),
     datetime(2016, 6, 2).strftime("%Y-%m-%d"), datetime(2016, 6, 19).strftime("%Y-%m-%d")))
 seasons.append(Season("2016–17", datetime(2016, 10, 25).strftime("%Y-%m-%d"), datetime(2017, 4, 12).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2017, 4, 15).strftime("%Y-%m-%d"), datetime(2017, 5, 25).strftime("%Y-%m-%d"),
     datetime(2017, 6, 1).strftime("%Y-%m-%d"), datetime(2017, 6, 12).strftime("%Y-%m-%d")))
 seasons.append(Season("2017–18", datetime(2017, 10, 17).strftime("%Y-%m-%d"), datetime(2018, 4, 11).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2018, 4, 14).strftime("%Y-%m-%d"), datetime(2018, 5, 28).strftime("%Y-%m-%d"),
     datetime(2018, 5, 31).strftime("%Y-%m-%d"), datetime(2018, 6, 8).strftime("%Y-%m-%d")))
 seasons.append(Season("2018–19", datetime(2018, 10, 16).strftime("%Y-%m-%d"), datetime(2019, 4, 10).strftime("%Y-%m-%d"),
+    None, None,
     datetime(2019, 4, 13).strftime("%Y-%m-%d"), datetime(2019, 5, 25).strftime("%Y-%m-%d"),
     datetime(2019, 5, 30).strftime("%Y-%m-%d"), datetime(2019, 6, 13).strftime("%Y-%m-%d")))
-seasons.append(Season("2019–20", datetime(2019, 10, 22).strftime("%Y-%m-%d"), datetime(2020, 8, 15).strftime("%Y-%m-%d"),
+seasons.append(Season("2019–20", datetime(2019, 10, 22).strftime("%Y-%m-%d"), datetime(2020, 3, 11).strftime("%Y-%m-%d"),
+    datetime(2020, 7, 30).strftime("%Y-%m-%d"), datetime(2020, 8, 15).strftime("%Y-%m-%d"),
     datetime(2020, 8, 17).strftime("%Y-%m-%d"), datetime(2020, 9, 27).strftime("%Y-%m-%d"),
     datetime(2020, 9, 30).strftime("%Y-%m-%d"), datetime(2020, 10, 11).strftime("%Y-%m-%d")))
-seasons.append(Season("2020–21", datetime(2020, 12, 22).strftime("%Y-%m-%d"), datetime(2021, 5, 21).strftime("%Y-%m-%d"),
+seasons.append(Season("2020–21", datetime(2020, 12, 22).strftime("%Y-%m-%d"), datetime(2021, 5, 16).strftime("%Y-%m-%d"),
+    datetime(2021, 5, 18).strftime("%Y-%m-%d"), datetime(2021, 5, 21).strftime("%Y-%m-%d"),
     datetime(2021, 5, 22).strftime("%Y-%m-%d"), datetime(2021, 7, 6).strftime("%Y-%m-%d"),
     datetime(2021, 7, 8).strftime("%Y-%m-%d"), datetime(2021, 7, 22).strftime("%Y-%m-%d")))
+seasons.append(Season("2021–22", datetime(2021, 10, 19).strftime("%Y-%m-%d"), datetime(2022, 4, 10).strftime("%Y-%m-%d"),
+    datetime(2022, 4, 12).strftime("%Y-%m-%d"), datetime(2022, 4, 15).strftime("%Y-%m-%d"),
+    datetime(2022, 4, 16).strftime("%Y-%m-%d"), datetime(2022, 5, 30).strftime("%Y-%m-%d"),
+    datetime(2022, 6, 2).strftime("%Y-%m-%d"), datetime(2022, 6, 19).strftime("%Y-%m-%d")))
 
-insert_command = "INSERT INTO season VALUES (?, ?, ?, ?, ?, ?, ?)"
+insert_command = "INSERT INTO season VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 for season in seasons:
     db_cursor.execute(insert_command,
         (season.name, season.startdate_rs, season.enddate_rs, season.startdate_po, season.enddate_po, season.startdate_f, season.enddate_f))
